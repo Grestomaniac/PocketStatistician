08663d61849864a9ce7c1779e6d1302c3ddd45b6
Darkhan <darkhan.vasilev.2016@mail.ru>
2020-05-04 17:39:16 +0900

    Added AutoCompleteTextView(2nd take)

---
+++ app/src/main/java/com/example/pocketstatistician/NewTypeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pocketstatistician
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewTypeActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+
+    }
+}
